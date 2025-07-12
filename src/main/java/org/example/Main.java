63091c9848ea1efbcb7566b00e6889e901ba196a
Stanislav <Stanislav.Petrov@russianpost.ru>
2025-07-12 12:11:12 +0400

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= 7; i++) {
 
             System.out.println("i = " + i);
         }
